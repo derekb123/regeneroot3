d6985a574dde1478a7d36ad11915e27a64748ede
derekb123 <derekbutvin@gmail.com>
2021-02-05 00:11:24 -0500

    second attempt at middleware that adds allow-origin to get out of cors error

--- server/server.js
+++ server/server.js
@@ -15,8 +15,17 @@ const app = express();
 const morgan = require("morgan");
 const cookieSession = require("cookie-session");
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
   next();
 });
 
